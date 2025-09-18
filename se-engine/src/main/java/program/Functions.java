6b2856823dd8f2e650520c4770deea49d36a026f
fabershaun <fabershaun@gmail.com>
2025-09-18 07:43:00 +0300

    add Functions: class that hold the manages the functions in the program (external from program)

---
+++ se-engine/src/main/java/program/Functions.java
@@ -0,0 +1,21 @@
+package program;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Functions {
+    private final Map<String, Program> nameToFunction = new HashMap<>();;
+
+    public void addFunction(String functionName, Program function) {
+        this.nameToFunction.put(functionName, function);
+    }
+
+    public Program getFunctionByName(String name) {
+        return this.nameToFunction.get(name);
+    }
+
+    public Collection<Program> getFunctions() {
+        return nameToFunction.values();
+    }
+}
--- se-engine/src/main/java/program/ProgramImpl.java
+++ se-engine/src/main/java/program/ProgramImpl.java
@@ -5,26 +5,11 @@ import java.util.*;
 
 public class ProgramImpl extends AbstractProgram {
 
-    private final Map<String, FunctionImpl> nameToFunction;
+    private final Functions functions = new Functions();;
 
     public ProgramImpl(String programOfFunctionName) {
         super(programOfFunctionName);
-        nameToFunction = new HashMap<>();
-    }
 
-    public void addInnerFunction(String functionName, FunctionImpl function) {
-        this.nameToFunction.put(functionName, function);
     }
-
-    public FunctionImpl getFunctionByName(String name) {
-        return this.nameToFunction.get(name);
-    }
-
-    public Collection<FunctionImpl> getFunctions() {
-        return nameToFunction.values();
-    }
-
     // TODO: עכשיו לכתוב את המתודה שבונה את רשימת תתי התוכניות
-
-
 }
