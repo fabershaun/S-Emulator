1b15ad390b21450299970b722406679ba84ed1f8
fabershaun <fabershaun@gmail.com>
2025-10-05 10:24:00 +0300

    Wrote getEngine()

--- se-WebApp/src/utils/ServletUtils.java
+++ se-WebApp/src/utils/ServletUtils.java
@@ -1,4 +1,21 @@
 package utils;
 
+import engine.Engine;
+import engine.EngineImpl;
+import jakarta.servlet.ServletContext;
+
 public class ServletUtils {
+
+    private static final String ENGINE_ATTRIBUTE_NAME = "engine";
+    private static final Object engineLock = new Object();
+
+    public static Engine getEngine(ServletContext servletContext) {
+
+        synchronized (engineLock) {
+            if (servletContext.getAttribute(ENGINE_ATTRIBUTE_NAME) == null) {
+                servletContext.setAttribute(ENGINE_ATTRIBUTE_NAME, new EngineImpl());
+            }
+        }
+        return (Engine) servletContext.getAttribute(ENGINE_ATTRIBUTE_NAME);
+    }
 }
