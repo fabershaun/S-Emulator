6af6fbb44d9932993b9e8cf01dffc8adf69e7619
fabershaun <fabershaun@gmail.com>
2025-10-07 13:29:11 +0300

    fix the dashboard.fxml to sub components

---
+++ se-client/src/components/dashboard/availablePrograms/AvailableProgramsController.java
@@ -0,0 +1,18 @@
+package components.dashboard.availablePrograms;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class AvailableProgramsController {
+    @FXML private Button executeProgramsButton;
+    @FXML private Label headerLabel;
+    @FXML private ListView<String> itemsListView;
+
+
+    @FXML
+    void onExecuteProgramsButtonClicked() {
+
+    }
+}
